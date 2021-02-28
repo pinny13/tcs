b37492c8d142fabc504c18a2d654ad1420ecc497
Pavel Kheyfets <kheyfets@amazon.com>
2021-02-27 22:42:05 -0800

    Creating soon page

---
+++ src/SoonPage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import "./common/variables.scss";
+
+const SoonPage = () => {
+    return (
+        <div className="container container-table">
+            <div className="row vertical-center-row">
+                <div className="text-center col-md-12 col-md-offset-12">
+                    <Jumbotron fluid style={{ color: "#8FC31F" }}>
+                        <h1>The website is coming soon</h1>
+                        <p>For now, please call Tonya at (425) 470-3767</p>
+                    </Jumbotron>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SoonPage;
